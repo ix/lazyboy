3cd64f6c5977b3a3b39c7e721fcf1af274f35f3f
Rose <rose@lain.org.uk>
2019-04-24 06:09:34 +0100

    change the names of the opcodes to better match the pandoc specifications

--- src/Lazyboy.hs
+++ src/Lazyboy.hs
@@ -21,24 +21,37 @@ data Register16 = BC | DE | HL
 
 -- | GB Opcodes
 data Opcode =
-    LDreg Register8 Register8 -- LD r8,r8
-  | LDimm Register8 Word8     -- LD r8, n8
-  | LD16imm Register16 Word16 -- LD r16, n16
-  | LDHLreg Register8         -- LD [HL], r8
-  | LDHLimm Word8             -- LD [HL], n8
-  | LDregHL Register8         -- LD r8, [HL]
+    LDrr Register8 Register8 -- load the value in one register8 into another
+  | LDrn Register8 Word8     -- load the immediate value8 into a register8
+  | LDrHL Register8          -- load the value8 stored at the address in HL into a register8
+  | LDHLr Register8          -- load the value8 stored in a register8 into the address in HL
+  | LDHLn Word8              -- load the immediate value8 into the address in HL
+  | LDABC                    -- load the value8 stored in the address in BC into A
+  | LDADE                    -- load the value8 stored in the address in DE into A
+  | LDAnn Word16             -- load the value8 stored in the value16 address into A
+  | LDBCA                    -- load the value8 stored in A into the address in BC 
+  | LDDEA                    -- load the value8 stored in A into the address in DE
+  | LDnnA                    -- load the value8 stored in A into the value16 address
 
-{-
-  LD [r16],A
-  LD [n16],A
-  LD [$FF00+n8],A
-  LD [$FF00+C],A
-  LD A,[r16]
-  LD A,[n16]
-  LD A,[$FF00+n8]
-  LD A,[$FF00+C]
-  LD [HL+],A
-  LD [HL-],A
-  LD A,[HL+]
-  LD A,[HL-]  -}
+  | LDHLnn Word16            -- load the value16 address into the register16 HL
+{- 
+  GMB 8bit load commands
+
+  ld   A,(FF00+n)  F0 nn     12 ---- read from io-port n (memory FF00+n)
+  ld   (FF00+n),A  E0 nn     12 ---- write to io-port n (memory FF00+n)
+  ld   A,(FF00+C)  F2         8 ---- read from io-port C (memory FF00+C)
+  ld   (FF00+C),A  E2         8 ---- write to io-port C (memory FF00+C)
+  ldi  (HL),A      22         8 ---- (HL)=A, HL=HL+1
+  ldi  A,(HL)      2A         8 ---- A=(HL), HL=HL+1
+  ldd  (HL),A      32         8 ---- (HL)=A, HL=HL-1
+  ldd  A,(HL)      3A         8 ---- A=(HL), HL=HL-1 
+  
+  
+GMB 16bit-Loadcommands
+  ld   rr,nn       x1 nn nn  12 ---- rr=nn (rr may be BC,DE,HL or SP)
+  ld   SP,HL       F9         8 ---- SP=HL
+  push rr          x5        16 ---- SP=SP-2  (SP)=rr   (rr may be BC,DE,HL,AF)
+  pop  rr          x1        12 (AF) rr=(SP)  SP=SP+2   (rr may be BC,DE,HL,AF)
+-}
+
   deriving (Read, Eq)
