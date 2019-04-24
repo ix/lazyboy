25a52e5194398732f7baefdb8fbd3ad205440659
Rose <rose@lain.org.uk>
2019-04-24 06:09:50 +0100

    rewrite the IO functions to use the new opcode names

--- src/Lazyboy/IO.hs
+++ src/Lazyboy/IO.hs
@@ -6,8 +6,12 @@ import           Lazyboy
 
 -- | Loads an 8 bit immediate value into an 8 bit register
 byte :: Register8 -> Word8 -> Writer [Opcode] ()
-byte reg val = tell [LDimm reg val]
+byte reg val = tell [LDrn reg val]
 
 -- | Loads an 8 immediate value into a 16 bit memory address
 write :: Word16 -> Word8 -> Writer [Opcode] ()
-write addr val = tell [LD16imm HL addr, LDHLimm val]
+write addr val = tell [LDHLnn addr, LDHLn val] 
+
+memcpy :: Word16 -> Word16 -> Writer [Opcode] ()
+memcpy src dest = do
+    tell []
