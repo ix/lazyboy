d3b48c2eeb39704677b682f3071adc0b96dc663d
Rose <rose@lain.org.uk>
2019-04-27 02:16:57 +0100

    simplify io.hs

--- src/Lazyboy/IO.hs
+++ src/Lazyboy/IO.hs
@@ -6,13 +6,11 @@ import           Lazyboy
 
 -- | Loads an 8 bit immediate value into an 8 bit register
 byte :: Register8 -> Word8 -> Lazyboy ()
-byte reg val = do
-    tell [LDrn reg val]
+byte reg val = tell [LDrn reg val]
 
 -- | Loads an 8 immediate value into a 16 bit memory address
 write :: Word16 -> Word8 -> Lazyboy ()
-write addr val = do
-    tell [LDrrnn HL addr, LDHLn val] 
+write addr val = tell [LDrrnn HL addr, LDHLn val] 
 
 memcpy :: Word16 -> Word16 -> Lazyboy ()
 memcpy src dest = do
