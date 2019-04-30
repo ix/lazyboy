6016737f1b9035d2f7350f2887d00742c098bac9
Rose <rose@lain.org.uk>
2019-04-30 18:47:19 +0100

    basic grammar fix

--- src/Lazyboy/IO.hs
+++ src/Lazyboy/IO.hs
@@ -4,11 +4,11 @@ import           Control.Monad.Trans.RWS.Lazy
 import           Data.Word
 import           Lazyboy
 
--- | Loads an 8 bit immediate value into an 8 bit register
+-- | Loads an 8-bit immediate value into an 8-bit register
 byte :: Register8 -> Word8 -> Lazyboy ()
 byte reg val = tell [LDrn reg val]
 
--- | Loads an 8 immediate value into a 16 bit memory address
+-- | Loads an 8-bit immediate value into a 16-bit memory address
 write :: Word16 -> Word8 -> Lazyboy ()
 write addr val = tell [LDrrnn HL addr, LDHLn val] 
 
