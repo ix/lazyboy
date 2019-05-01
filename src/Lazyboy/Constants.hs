eeca71b5372b900c806f24a3089c8634f931c205
Rose <rose@lain.org.uk>
2019-05-01 20:42:00 +0100

    add more constants and order them better

--- src/Lazyboy/Constants.hs
+++ src/Lazyboy/Constants.hs
@@ -14,18 +14,6 @@ module Lazyboy.Constants where
 
 import           Data.Word
 
--- | Background Palette Data
-bgp :: Word16
-bgp = 0xFF47
-
--- | LCDC Y-coordinate
-ly :: Word16
-ly = 0xFF44
-
--- | LCD Control
-lcdc :: Word16
-lcdc = 0xFF40
-
 -- | Work RAM (WRAM) Bank 0
 wram0 :: Word16
 wram0 = 0xC000
@@ -34,6 +22,14 @@ wram0 = 0xC000
 wram1 :: Word16
 wram1 = 0xD000
 
+-- | LCD Control
+lcdc :: Word16
+lcdc = 0xFF40
+
+-- | LCD state 
+lcdstate :: Word16
+lcdstate = 0xFF41
+
 -- | Scroll X
 scx :: Word16
 scx = 0xFF42
@@ -41,6 +37,22 @@ scx = 0xFF42
 -- | Scroll Y
 scy :: Word16
 scy = 0xFF43
+
+-- | LCDC Y-coordinate
+ly :: Word16
+ly = 0xFF44
+
+-- | LCDC Y-compare
+lyc :: Word16
+lyc = 0xFF45
+
+-- | DMA start address
+dma :: Word16 
+dma = 0xFF46
+
+-- | Background Palette Data
+bgp :: Word16
+bgp = 0xFF47
 
 -- | Video RAM (VRAM)
 vram :: Word16
@@ -58,5 +70,14 @@ background2 = 0x9C00
 hram :: Word16
 hram = 0xFF80
 
+-- | OAM 
 oam :: Word16
 oam = 0xFE00
+
+-- | Screen width
+screenWidth :: Word8
+screenWidth = 160
+
+-- | Screen height
+screenHeight :: Word8
+screenHeight = 144
