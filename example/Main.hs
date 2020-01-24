c38ba0ed3d2bafed59de0285728e8b30d34edb60
Rose <rose@empty.town>
2020-01-24 22:40:29 +0000

    revamp example

--- example/Main.hs
+++ example/Main.hs
@@ -1,17 +1,21 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-14.21 script --package lazyboy
+
 module Main where
 
-import qualified Data.Text.Lazy.IO  as T
-import           Data.Word
-import           Lazyboy
-import           Lazyboy.Prelude
-import           Lazyboy.Target.ASM
-import           Prelude            hiding ((&&), (/=), (<), (==), (>), (||))
+
+import Data.Word          (Word8)
+import Lazyboy
+import Lazyboy.Prelude
+import Lazyboy.Target.ASM
+import Prelude            hiding ((&&), (/=), (<), (==), (>), (||))
+
+import qualified Data.Text.IO as T
 
 main :: IO ()
-main = rom >>= T.putStrLn
-    where rom = compileROM $ do
-            byte A 0xDE
-            byte B 0xDE
-            if' ((A == (0xDE :: Word8)) && (A == B)) $
-                write (Address wram0) 0xDE
-            freeze
+main = T.putStrLn $ compileROM $ do
+  byte A 0xDE
+  byte B 0xDE
+  if' ((A == (0xDE :: Word8)) && (A == B)) $
+    write (Address wram0) 0xDE
+  freeze
